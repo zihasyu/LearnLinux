4be78a98f1ca85afa60402a88748d98c753e08ed
zihasyu <1045839067@qq.com>
2024-11-26 15:36:23 +0800

    adjust some pos

--- Homework1/setup.sh
+++ Homework1/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p ./bin
-g++ ./src/main.cc ./src/FileStatus.cc ./src/FileThread.cc ./src/FileThreadRW.cc ./src/FileWithCache.cc -o ./bin/FileWithCacheTest -std=c++11 -O3 -Wall -lpthread -g -fsanitize=address
+g++ ./src/main.cc ./src/FileStatus.cc ./src/FileThread.cc ./src/FileThreadRW.cc ./src/FileWithCache.cc -o ./bin/FileCache -std=c++11 -O3 -Wall -lpthread -g -fsanitize=address
 echo "Compilation completed"
