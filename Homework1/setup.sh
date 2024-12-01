5352ed999be0c03ccb0ebd5d987bec4c02e8499d
zihasyu <1045839067@qq.com>
2024-12-01 14:50:22 +0800

    add some shell

--- Homework1/setup.sh
+++ Homework1/setup.sh
@@ -5,3 +5,5 @@ cd ..
 mkdir -p ./bin
 g++ ./src/main.cc ./src/FileStatus.cc ./src/FileThread.cc ./src/FileThreadRW.cc ./src/FileWithCache.cc -o ./bin/FileCache -std=c++11 -O3 -Wall -lpthread -g -fsanitize=address
 echo "Compilation completed"
+cd bin
+./FileCache
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LearnLinux
 Only as Ziyu Wang's assignments
+
+All programs can be compiled and tested by running setup.sh
